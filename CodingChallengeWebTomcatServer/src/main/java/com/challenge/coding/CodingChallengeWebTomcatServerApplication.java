552973911b0121634cfb41007cc37644c26125b0
ken <plcnkl@msn.com>
2018-02-20 09:07:22 +0800

    Initial import of CodingChallengeWebTomcatServer

---
+++ CodingChallengeWebTomcatServer/src/main/java/com/challenge/coding/CodingChallengeWebTomcatServerApplication.java
@@ -0,0 +1,12 @@
+package com.challenge.coding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingChallengeWebTomcatServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingChallengeWebTomcatServerApplication.class, args);
+	}
+}
